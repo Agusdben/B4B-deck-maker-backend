2f59759dfccfc409147b4fdda41fc50dbda04f53
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-10 14:32:43 -0300

    changed how works affinities img

--- src/controllers/cards.controller.js
+++ src/controllers/cards.controller.js
@@ -1,14 +1,16 @@
 import * as Cards from '../services/cards.service.js'
-import createImageUrl from '../helpers/createImageUrl.js'
 
 export const getCards = async (req, res, next) => {
   try {
     const cards = await Cards.findAll()
+
+    const baseURL = `${req.protocol}://${req.get('host')}/api`
+
     const cardsToReturn = cards.map((c) => {
       return {
         ...c,
-        img: `${req.protocol}://${req.get('host')}/api/images/${c.img}`,
-        affinity_img: createImageUrl(c.affinity_img)
+        img: `${baseURL}/images/${c.img}`,
+        affinity_img: `${baseURL}/images/${c.affinity_img}`
       }
     })
     return res.status(200).json(cardsToReturn).end()
--- src/helpers/createImageUrl.js
+++
@@ -1,4 +0,0 @@
-export default function createImageUrl (img) {
-  const buffer = Buffer.from(img)
-  return `data:image/png;base64,${buffer.toString('base64')}`
-}

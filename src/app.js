2cdbbbeaf54b7183e4d9cc57dfac5325edd95ed2
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-12 15:30:29 -0300

    added CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import db from './config/db.js'
+import cors from 'cors'
 
 // routes
 import usersRoutes from './routes/users.route.js'
@@ -9,10 +10,11 @@ import imagesRoutes from './routes/images.route.js'
 import decksRoutes from './routes/decks.route.js'
 import decksCards from './routes/decks_cards.route.js'
 import { handleErrors } from './middlewares/handleErrors.js'
+import { WHITE_LIST } from './constants/whiteList.js'
 
 const app = express()
 app.use(express.json())
-
+app.use(cors({ origin: WHITE_LIST }))
 dotenv.config()
 
 const PORT = process.env.PORT || 3000

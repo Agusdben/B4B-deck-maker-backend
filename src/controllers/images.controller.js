9dab72dec9de85c2af2859275786cf2e2a349382
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-23 10:03:41 -0300

    removed image extension

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -3,10 +3,9 @@ import fs from 'fs'
 export const getImage = async (req, res, next) => {
   try {
     const { imageName } = req.params
-    const extension = imageName.split('.').at(-1)
     const imgRoute = `./src/files/images/${imageName}`
     const image = fs.readFileSync(imgRoute)
-    res.writeHead(200, { 'Content-Type': `image/${extension}` })
+    res.writeHead(200, { 'Content-Type': 'image/*' })
     res.end(image, 'binary')
   } catch (error) {
     next(error)

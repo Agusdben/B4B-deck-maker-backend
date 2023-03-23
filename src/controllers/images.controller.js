ed7b469530838a938f819ae27a2e8d3a634dca18
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-23 10:09:27 -0300

    added extension with toString

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -3,9 +3,11 @@ import fs from 'fs'
 export const getImage = async (req, res, next) => {
   try {
     const { imageName } = req.params
+    console.log(imageName)
+    const extension = imageName.toString().split('.').at(-1)
     const imgRoute = `./src/files/images/${imageName}`
     const image = fs.readFileSync(imgRoute)
-    res.writeHead(200, { 'Content-Type': 'image/*' })
+    res.writeHead(200, { 'Content-Type': `image/${extension}` })
     res.end(image, 'binary')
   } catch (error) {
     next(error)
